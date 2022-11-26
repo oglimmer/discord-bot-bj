8cc5c582faaf5ed52e9d81be44e86f7581bd9354
Oli Zimpasser <oglimmer@gmail.com>
2022-11-26 16:11:20 +0100

    Send hidden messages

--- index.js
+++ index.js
@@ -78,28 +78,28 @@ client.on('interactionCreate', async interaction => {
 	try {
 		if (commandName === 'highscore') {
 			const data = await got.get(`${SERVER_ROOT}/v2/highscore`).json();
-			await interaction.reply(JSON.stringify(data, null, 2));
+			await interaction.reply({ content: JSON.stringify(data, null, 2), ephemeral: true});
 		}
 		else if (commandName === 'create-player') {
 			const url = `${SERVER_ROOT}/v2/player`;
 			const data = await got.post(url, { json: { 'name': interaction.user.tag } }).json();
 			if (!store[interaction.user.tag])store[interaction.user.tag] = {};
 			store[interaction.user.tag].playerId = data.playerId;
-			await interaction.reply('Player created. You have $1000.');
+			await interaction.reply({ content: 'Player created. You have $1000.', ephemeral: true});
 		}
 		else if (commandName === 'get-player') {
 			const data = await got.get(`${SERVER_ROOT}/v2/player/${store[interaction.user.tag].playerId}`).json();
-			await interaction.reply(`You have \\$${data.cash}`);
+			await interaction.reply({ content: `You have \\$${data.cash}`, ephemeral: true});
 		}
 		else if (commandName === 'create-deck') {
 			const data = await got.post(`${SERVER_ROOT}/v2/deck`).json();
 			if (!store[interaction.user.tag])store[interaction.user.tag] = {};
 			store[interaction.user.tag].deckId = data.deckId;
-			await interaction.reply('New deck created and shuffled. It contains 6x 52 cards.');
+			await interaction.reply({ content: 'New deck created and shuffled. It contains 6x 52 cards.', ephemeral: true});
 		}
 		else if (commandName === 'bet') {
 			if (!store[interaction.user.tag] || !store[interaction.user.tag].deckId) {
-				await interaction.reply("You haven't created a deck yet.");
+				await interaction.reply({ content: "You haven't created a deck yet.", ephemeral: true});
 				return
 			}
 			const gameData = await got.post(`${SERVER_ROOT}/v2/game`, { json: { 'deckId': store[interaction.user.tag].deckId } }).json();
@@ -110,30 +110,30 @@ client.on('interactionCreate', async interaction => {
 			store[interaction.user.tag].betId = betData.betId;
 			const { card1, card2, dealersCard, yourTotal, followActions } = betData;
 			if (followActions.length == 0) {
-				await interaction.reply(`A game has started and your bet has been placed. Your cards: ${card1} and ${card2}. The dealer's open card ${dealersCard}. Your total is ${yourTotal}. ` + await evalResult(interaction));
+				await interaction.reply({ content: `A game has started and your bet has been placed. Your cards: ${card1} and ${card2}. The dealer's open card ${dealersCard}. Your total is ${yourTotal}. ` + await evalResult(interaction), ephemeral: true});
 			}
 			else {
-				await interaction.reply(`A game has started and your bet has been placed. Your cards: ${card1} and ${card2}. The dealer's open card ${dealersCard}. Your total is ${yourTotal}. Your options are ${followActions}.`);
+				await interaction.reply({ content: `A game has started and your bet has been placed. Your cards: ${card1} and ${card2}. The dealer's open card ${dealersCard}. Your total is ${yourTotal}. Your options are ${followActions}.`, ephemeral: true});
 			}
 		}
 		else if (commandName === 'hit') {
 			const data = await got.post(`${SERVER_ROOT}/v2/game/${store[interaction.user.tag].gameId}/bet/${store[interaction.user.tag].betId}/hit`).json();
 			const { drawnCard, yourTotal, followActions } = data;
 			if (data.followActions.length == 0) {
-				await interaction.reply(`You have drawn ${drawnCard}, which brings your total to ${yourTotal}. ` + await evalResult(interaction));
+				await interaction.reply({ content: `You have drawn ${drawnCard}, which brings your total to ${yourTotal}. ` + await evalResult(interaction), ephemeral: true});
 			}
 			else {
-				await interaction.reply(`You have drawn ${drawnCard}, which brings your total to ${yourTotal}. Your options are ${followActions}.`);
+				await interaction.reply({ content: `You have drawn ${drawnCard}, which brings your total to ${yourTotal}. Your options are ${followActions}.`, ephemeral: true});
 			}
 		}
 		else if (commandName === 'stand') {
 			await got.post(`${SERVER_ROOT}/v2/game/${store[interaction.user.tag].gameId}/bet/${store[interaction.user.tag].betId}/stand`).json();
-			await interaction.reply(await evalResult(interaction));
+			await interaction.reply({ content: await evalResult(interaction), ephemeral: true});
 		}
 		else if (commandName === 'double') {
 			const data = await got.post(`${SERVER_ROOT}/v2/game/${store[interaction.user.tag].gameId}/bet/${store[interaction.user.tag].betId}/double`).json();
 			const { drawnCard, yourTotal } = data;
-			await interaction.reply(`You have drawn ${drawnCard}, which brings your total to ${yourTotal}. ` + await evalResult(interaction));
+			await interaction.reply({ content: `You have drawn ${drawnCard}, which brings your total to ${yourTotal}. ` + await evalResult(interaction), ephemeral: true});
 		}
 		else if (commandName === 'split') {
 			const data = await got.post(`${SERVER_ROOT}/v2/game/${store[interaction.user.tag].gameId}/bet/${store[interaction.user.tag].betId}/split`).json();
@@ -141,31 +141,31 @@ client.on('interactionCreate', async interaction => {
 			const { firstBetCard1, firstBetCard2, firstBetTotal, secondBetCard1, secondBetCard2, secondBetTotal, followActions, secondBetFollowAction } = betData;
 			if (followActions.length == 0) {
 				if (secondBetFollowAction.length == 0) {
-					await interaction.reply('both hands are done. call /result');
+					await interaction.reply({ content: 'both hands are done. call /result', ephemeral: true});
 				} else {
-					await interaction.reply(`Your first hand is ${firstBetCard1} and ${firstBetCard2}, with a total of ${firstBetTotal}. Your second hand is ${secondBetCard1} and ${secondBetCard2}, with a total of ${secondBetTotal}. No actions available for first hand. The second had can do ${secondBetFollowAction}.`);
+					await interaction.reply({ content: `Your first hand is ${firstBetCard1} and ${firstBetCard2}, with a total of ${firstBetTotal}. Your second hand is ${secondBetCard1} and ${secondBetCard2}, with a total of ${secondBetTotal}. No actions available for first hand. The second had can do ${secondBetFollowAction}.`, ephemeral: true});
 				}
 			}
 			else {
-				await interaction.reply(`Your first hand is ${firstBetCard1} and ${firstBetCard2}, with a total of ${firstBetTotal}. Your second hand is ${secondBetCard1} and ${secondBetCard2}, with a total of ${secondBetTotal}. Your options for the first hand are ${followActions}, for the second had ${secondBetFollowAction}.`);
+				await interaction.reply({ content: `Your first hand is ${firstBetCard1} and ${firstBetCard2}, with a total of ${firstBetTotal}. Your second hand is ${secondBetCard1} and ${secondBetCard2}, with a total of ${secondBetTotal}. Your options for the first hand are ${followActions}, for the second had ${secondBetFollowAction}.`, ephemeral: true});
 			}
 		}
 		else if (commandName === 'result') {
 			const data = await got.post(`${SERVER_ROOT}/v2/game/${store[interaction.user.tag].gameId}/bet/${store[interaction.user.tag].betId}`).json();
-			await interaction.reply(await evalResult(JSON.stringify(data)));
+			await interaction.reply({ content: await evalResult(JSON.stringify(data)), ephemeral: true});
 		}
 		else if (commandName === 'insurance') {
 			const insurance = interaction.options.getInteger('insurance');
 			const data = await got.post(`${SERVER_ROOT}/v2/game/${store[interaction.user.tag].gameId}/bet/${store[interaction.user.tag].betId}/insurance`, { json: { 'insurance': insurance } }).json();
 			const { followActions } = data;
-			await interaction.reply(`Your options are ${followActions}.`);
+			await interaction.reply({ content: `Your options are ${followActions}.`, ephemeral: true});
 		}
 	} catch (error) {
 		console.log(error);
 		if (error.response) {
-			await interaction.reply("Error: " + error.response.body);
+			await interaction.reply({ content: "Error: " + error.response.body, ephemeral: true});
 		} else {
-			await interaction.reply("Error: " + JSON.stringify(error));
+			await interaction.reply({ content: "Error: " + JSON.stringify(error), ephemeral: true});
 		}
 	}
 });
